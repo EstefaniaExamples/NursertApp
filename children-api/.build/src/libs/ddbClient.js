9ccf522cd16a3291bd494fdc3182f1fdb6c6ed53
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2022-09-22 16:06:57 +0200

    Delete children-api/.build/src directory

--- children-api/.build/src/libs/ddbClient.js
+++
@@ -1,4 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-const REGION = "eu-west-2";
-export const ddbClient = new DynamoDBClient({ region: REGION });
-//# sourceMappingURL=ddbClient.js.map
