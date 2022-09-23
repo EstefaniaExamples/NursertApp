292d8f5e88bbf527b10aeab11ed29909e9b057c3
FanyCastro <estefania.castro.vizoso@gmail.com>
2022-09-23 13:19:22 +0200

    feat: children put working

--- children-api/src/functions/children.ts
+++ children-api/src/functions/children.ts
@@ -1,75 +1,82 @@
 import { APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda';
 import middy from '@middy/core';
-import { QueryCommand } from '@aws-sdk/client-dynamodb';
+import { ScanCommand } from '@aws-sdk/client-dynamodb';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbClient } from '../libs/ddbClient.js';
 import { ddbDocClient } from '../libs/ddbDocClient.js';
-
+import { kidSchema } from '../model/kid_model.js';
+import { ZodError } from 'zod';
+import { v4 as uuidv4 } from 'uuid';
 
 const childrenPutFunction = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> =>{
     console.log(event.body)
-    const newKidParams = {
-        TableName: "children-api-dev",
-        Item: {
-            "artist":  "song.artist",
-            "song": "song.song",
-            "id":  "song.id",
-            "priceUsdCents": "song.priceUsdCents",
-            "publisher": "song.publisher"
-        },
-      };
 
-    const data = await ddbDocClient.send(new PutCommand(newKidParams));
-    console.log("Success - item added or updated", data);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'New children added',
-                input: event,
+    try { 
+        const item = kidSchema.parse(JSON.parse(event.body!))
+        const newKidParams = {
+            TableName: "children-api-dev",
+            Item: {
+                "KidId": uuidv4(),
+                "KidName": item.KidName,
+                "KidSurname":  item.KidSurname,
+                "BirthDate": item.BirthDate,
+                "Address": item.Address
             },
-            null,
-            2
-        ),
-    };
-}  
+          };
+    
+        const data = await ddbDocClient.send(new PutCommand(newKidParams));
+        console.log("Success - item added or updated", data);
+    
+        return {
+            statusCode: 201,
+            body: JSON.stringify( {
+                    item: item,
+                }, null, 2),
+            headers: { "Content-Type": "application/json" },
+        };
+    } catch (error) { 
+        console.log(error);
+        var message: String = "internal server error"
+        if (error instanceof ZodError) {
+            message = "Error in the request body."
+        }
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: message }, null, 2),
+            headers: { "Content-Type": "application/json" },
+        };
+    }        
+}
+    
 
 const childrenGetFunction = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> =>{
     console.log("INFO: Starting children handler")
     try {
         const getParams = {
-            KeyConditionExpression: 'KidId=:n',
-            ExpressionAttributeValues: {
-                ":n": { N: "0" },
-              },
             TableName: "children-api-dev",
         };
-        const data = await ddbClient.send(new QueryCommand(getParams));
+        const data = await ddbClient.send(new ScanCommand(getParams));
         const kidsList: String[] = [];
-        data.Items.forEach(function (element) {
-          kidsList.push(element.KidName.S + " " +  element.KidSurname.S + " ("  +  element.BirthDate.S + ")")
-        });
+        if (typeof data !== undefined) {
+            data.Items.forEach(function (element) {
+                kidsList.push(element.KidName.S + " " +  element.KidSurname.S + " ("  +  element.BirthDate.S + ")")
+              });
+        }
         return {
             statusCode: 200,
             body: JSON.stringify({
                     children: kidsList,
-                },
-                null,
-                2
-            ),
+                }, null, 2 ),
+            headers: { "Content-Type": "application/json"}
         };
     } catch (err) {
         return {
             statusCode: 500, 
-            body: JSON.stringify(
-                {
+            body: JSON.stringify( {
                     error: 'An error has ocurred',
                     message: err,
-                },
-                null,
-                2
-            ),
+                }, null, 2 ),
+            headers: { "Content-Type": "application/json"}
         };
     }
 }
