68c45861709bf62de5595f81873bc15cee6e3a11
BodneyC <benjcarrington@gmail.com>
2022-09-24 18:59:30 +0100

    feat(kid): little extra bit in the zod-ish

--- children-api/src/kid.ts
+++ children-api/src/kid.ts
@@ -1,15 +1,19 @@
+// No underscores in filenames please
+
 import { z } from "zod";
 
 export const kidSchema = z.object({
   KidName: z.string({
+    // Not entirely sure of the benefit of the custom error messages
     required_error: "Name is required",
     invalid_type_error: "Name must be a string",
-  }).min(3, { message: "Must be 3 or more characters long" }),
+  }).min(3,
+    // I also think this would be quite similar to the default :D
+    { message: "Must be 3 or more characters long" }),
   KidSurname: z.string(),
   BirthDate: z.string(),
   Address: z.string(),
 });
-
 
 // extract the inferred type
 export type KidType = z.infer<typeof kidSchema>;
