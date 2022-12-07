c9f9f8d9eadd5426d7a14d8ca3058cfa9ab12c06
FanyCastro <estefania.castro.vizoso@gmail.com>
2022-12-07 19:54:36 +0100

    webpack with commons working

---
+++ children-api/.webpack/service/src/children/get.js
@@ -0,0 +1,275 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/children/async-method.ts":
+/*!**************************************!*\
+  !*** ./src/children/async-method.ts ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.main = void 0;
+async function main() {
+    const value = await Promise.resolve('Hey there');
+    console.log('inside: ' + value);
+    return value;
+}
+exports.main = main;
+
+
+/***/ }),
+
+/***/ "./src/libs/baseError.ts":
+/*!*******************************!*\
+  !*** ./src/libs/baseError.ts ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.APIError = exports.HttpStatusCode = void 0;
+var HttpStatusCode;
+(function (HttpStatusCode) {
+    HttpStatusCode[HttpStatusCode["OK"] = 200] = "OK";
+    HttpStatusCode[HttpStatusCode["BAD_REQUEST"] = 400] = "BAD_REQUEST";
+    HttpStatusCode[HttpStatusCode["NOT_FOUND"] = 404] = "NOT_FOUND";
+    HttpStatusCode[HttpStatusCode["INTERNAL_SERVER"] = 500] = "INTERNAL_SERVER";
+})(HttpStatusCode = exports.HttpStatusCode || (exports.HttpStatusCode = {}));
+class BaseError extends Error {
+    constructor(name, httpCode, description, isOperational) {
+        super(description);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = name;
+        this.httpCode = httpCode;
+        this.isOperational = isOperational;
+        Error.captureStackTrace(this);
+    }
+}
+class APIError extends BaseError {
+    constructor(name, httpCode = HttpStatusCode.INTERNAL_SERVER, description = 'internal server error', isOperational = true) {
+        super(name, httpCode, description, isOperational);
+    }
+}
+exports.APIError = APIError;
+
+
+/***/ }),
+
+/***/ "./src/libs/dynamodb.ts":
+/*!******************************!*\
+  !*** ./src/libs/dynamodb.ts ***!
+  \******************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.ddbDocClient = exports.ddbClient = void 0;
+const lib_dynamodb_1 = __webpack_require__(/*! @aws-sdk/lib-dynamodb */ "@aws-sdk/lib-dynamodb");
+const client_dynamodb_1 = __webpack_require__(/*! @aws-sdk/client-dynamodb */ "@aws-sdk/client-dynamodb");
+const REGION = 'eu-west-2';
+function getDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+        console.info('Setting the configuration to use the localhost database');
+        return new client_dynamodb_1.DynamoDBClient({
+            region: 'localhost',
+            endpoint: 'http://localhost:5001',
+        });
+    }
+    console.info('Setting the configuration to use the AWS database');
+    return new client_dynamodb_1.DynamoDBClient({ region: REGION });
+}
+exports.ddbClient = getDynamoDBClient();
+const marshallOptions = {
+    convertEmptyValues: false,
+    removeUndefinedValues: false,
+    convertClassInstanceToMap: true,
+};
+const unmarshallOptions = {
+    wrapNumbers: false,
+};
+const translateConfig = { marshallOptions, unmarshallOptions };
+exports.ddbDocClient = lib_dynamodb_1.DynamoDBDocumentClient.from(exports.ddbClient, translateConfig);
+
+
+/***/ }),
+
+/***/ "./src/libs/repository.ts":
+/*!********************************!*\
+  !*** ./src/libs/repository.ts ***!
+  \********************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.saveChildren = exports.deleteChildrenById = exports.getChildrenById = exports.getAllChildren = void 0;
+const lib_dynamodb_1 = __webpack_require__(/*! @aws-sdk/lib-dynamodb */ "@aws-sdk/lib-dynamodb");
+const uuid_1 = __webpack_require__(/*! uuid */ "uuid");
+const baseError_1 = __webpack_require__(/*! ./baseError */ "./src/libs/baseError.ts");
+const dynamodb_1 = __webpack_require__(/*! ./dynamodb */ "./src/libs/dynamodb.ts");
+async function getAllChildren() {
+    const { Items } = await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.ScanCommand({
+        TableName: 'children-api-dev',
+        ConsistentRead: true,
+    }));
+    if (Items === undefined) {
+        return [];
+    }
+    else {
+        return Items;
+    }
+}
+exports.getAllChildren = getAllChildren;
+async function getChildrenById(id) {
+    const { Item } = await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.GetCommand({
+        TableName: 'children-api-dev',
+        Key: { KidId: id },
+    }));
+    if (Item == undefined) {
+        throw new baseError_1.APIError('NOT FOUND', baseError_1.HttpStatusCode.NOT_FOUND, `Item with id ${id} not found`, true);
+    }
+    return Item;
+}
+exports.getChildrenById = getChildrenById;
+async function deleteChildrenById(id) {
+    await getChildrenById(id);
+    await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.DeleteCommand({
+        TableName: 'children-api-dev',
+        Key: {
+            KidId: id,
+        },
+    }));
+}
+exports.deleteChildrenById = deleteChildrenById;
+async function saveChildren(item) {
+    await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.PutCommand({
+        TableName: 'children-api-dev',
+        Item: {
+            ...item,
+            KidId: (0, uuid_1.v4)(),
+        },
+    }));
+}
+exports.saveChildren = saveChildren;
+
+
+/***/ }),
+
+/***/ "./src/libs/util.ts":
+/*!**************************!*\
+  !*** ./src/libs/util.ts ***!
+  \**************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.formatJSONResponse = void 0;
+const formatJSONResponse = (response, statusCode = 200, headers = { 'Content-Type': 'application/json' }) => {
+    return {
+        statusCode,
+        body: JSON.stringify(response),
+        headers,
+    };
+};
+exports.formatJSONResponse = formatJSONResponse;
+
+
+/***/ }),
+
+/***/ "@aws-sdk/client-dynamodb":
+/*!*******************************************!*\
+  !*** external "@aws-sdk/client-dynamodb" ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = require("@aws-sdk/client-dynamodb");
+
+/***/ }),
+
+/***/ "@aws-sdk/lib-dynamodb":
+/*!****************************************!*\
+  !*** external "@aws-sdk/lib-dynamodb" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("@aws-sdk/lib-dynamodb");
+
+/***/ }),
+
+/***/ "uuid":
+/*!***********************!*\
+  !*** external "uuid" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("uuid");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!*****************************!*\
+  !*** ./src/children/get.ts ***!
+  \*****************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.handler = void 0;
+const util_1 = __webpack_require__(/*! @libs/util */ "./src/libs/util.ts");
+const async_method_1 = __webpack_require__(/*! ./async-method */ "./src/children/async-method.ts");
+const repository_1 = __webpack_require__(/*! @libs/repository */ "./src/libs/repository.ts");
+const asyncMsg = (async () => {
+    return await (0, async_method_1.main)();
+})();
+const getChildren = async () => {
+    console.info('INFO: Starting get all kids handler');
+    console.info('withing get method ' + await asyncMsg);
+    try {
+        const items = await (0, repository_1.getAllChildren)();
+        return (0, util_1.formatJSONResponse)({ kids: items });
+    }
+    catch (err) {
+        console.error(err);
+        return (0, util_1.formatJSONResponse)({ message: err.message }, 500);
+    }
+};
+const handler = async () => getChildren();
+exports.handler = handler;
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
+//# sourceMappingURL=get.js.map
---
+++ children-api/.webpack/service/src/children/put.js
@@ -0,0 +1,296 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/children/kid.ts":
+/*!*****************************!*\
+  !*** ./src/children/kid.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.kidSchema = void 0;
+const zod_1 = __webpack_require__(/*! zod */ "zod");
+exports.kidSchema = zod_1.z.object({
+    KidName: zod_1.z
+        .string({
+        required_error: 'Name is required',
+        invalid_type_error: 'Name must be a string',
+    })
+        .min(3, { message: 'Must be 3 or more characters long' }),
+    KidSurname: zod_1.z.string(),
+    BirthDate: zod_1.z.string(),
+    Address: zod_1.z.string(),
+});
+
+
+/***/ }),
+
+/***/ "./src/libs/baseError.ts":
+/*!*******************************!*\
+  !*** ./src/libs/baseError.ts ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.APIError = exports.HttpStatusCode = void 0;
+var HttpStatusCode;
+(function (HttpStatusCode) {
+    HttpStatusCode[HttpStatusCode["OK"] = 200] = "OK";
+    HttpStatusCode[HttpStatusCode["BAD_REQUEST"] = 400] = "BAD_REQUEST";
+    HttpStatusCode[HttpStatusCode["NOT_FOUND"] = 404] = "NOT_FOUND";
+    HttpStatusCode[HttpStatusCode["INTERNAL_SERVER"] = 500] = "INTERNAL_SERVER";
+})(HttpStatusCode = exports.HttpStatusCode || (exports.HttpStatusCode = {}));
+class BaseError extends Error {
+    constructor(name, httpCode, description, isOperational) {
+        super(description);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = name;
+        this.httpCode = httpCode;
+        this.isOperational = isOperational;
+        Error.captureStackTrace(this);
+    }
+}
+class APIError extends BaseError {
+    constructor(name, httpCode = HttpStatusCode.INTERNAL_SERVER, description = 'internal server error', isOperational = true) {
+        super(name, httpCode, description, isOperational);
+    }
+}
+exports.APIError = APIError;
+
+
+/***/ }),
+
+/***/ "./src/libs/dynamodb.ts":
+/*!******************************!*\
+  !*** ./src/libs/dynamodb.ts ***!
+  \******************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.ddbDocClient = exports.ddbClient = void 0;
+const lib_dynamodb_1 = __webpack_require__(/*! @aws-sdk/lib-dynamodb */ "@aws-sdk/lib-dynamodb");
+const client_dynamodb_1 = __webpack_require__(/*! @aws-sdk/client-dynamodb */ "@aws-sdk/client-dynamodb");
+const REGION = 'eu-west-2';
+function getDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+        console.info('Setting the configuration to use the localhost database');
+        return new client_dynamodb_1.DynamoDBClient({
+            region: 'localhost',
+            endpoint: 'http://localhost:5001',
+        });
+    }
+    console.info('Setting the configuration to use the AWS database');
+    return new client_dynamodb_1.DynamoDBClient({ region: REGION });
+}
+exports.ddbClient = getDynamoDBClient();
+const marshallOptions = {
+    convertEmptyValues: false,
+    removeUndefinedValues: false,
+    convertClassInstanceToMap: true,
+};
+const unmarshallOptions = {
+    wrapNumbers: false,
+};
+const translateConfig = { marshallOptions, unmarshallOptions };
+exports.ddbDocClient = lib_dynamodb_1.DynamoDBDocumentClient.from(exports.ddbClient, translateConfig);
+
+
+/***/ }),
+
+/***/ "./src/libs/repository.ts":
+/*!********************************!*\
+  !*** ./src/libs/repository.ts ***!
+  \********************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.saveChildren = exports.deleteChildrenById = exports.getChildrenById = exports.getAllChildren = void 0;
+const lib_dynamodb_1 = __webpack_require__(/*! @aws-sdk/lib-dynamodb */ "@aws-sdk/lib-dynamodb");
+const uuid_1 = __webpack_require__(/*! uuid */ "uuid");
+const baseError_1 = __webpack_require__(/*! ./baseError */ "./src/libs/baseError.ts");
+const dynamodb_1 = __webpack_require__(/*! ./dynamodb */ "./src/libs/dynamodb.ts");
+async function getAllChildren() {
+    const { Items } = await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.ScanCommand({
+        TableName: 'children-api-dev',
+        ConsistentRead: true,
+    }));
+    if (Items === undefined) {
+        return [];
+    }
+    else {
+        return Items;
+    }
+}
+exports.getAllChildren = getAllChildren;
+async function getChildrenById(id) {
+    const { Item } = await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.GetCommand({
+        TableName: 'children-api-dev',
+        Key: { KidId: id },
+    }));
+    if (Item == undefined) {
+        throw new baseError_1.APIError('NOT FOUND', baseError_1.HttpStatusCode.NOT_FOUND, `Item with id ${id} not found`, true);
+    }
+    return Item;
+}
+exports.getChildrenById = getChildrenById;
+async function deleteChildrenById(id) {
+    await getChildrenById(id);
+    await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.DeleteCommand({
+        TableName: 'children-api-dev',
+        Key: {
+            KidId: id,
+        },
+    }));
+}
+exports.deleteChildrenById = deleteChildrenById;
+async function saveChildren(item) {
+    await dynamodb_1.ddbDocClient.send(new lib_dynamodb_1.PutCommand({
+        TableName: 'children-api-dev',
+        Item: {
+            ...item,
+            KidId: (0, uuid_1.v4)(),
+        },
+    }));
+}
+exports.saveChildren = saveChildren;
+
+
+/***/ }),
+
+/***/ "./src/libs/util.ts":
+/*!**************************!*\
+  !*** ./src/libs/util.ts ***!
+  \**************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.formatJSONResponse = void 0;
+const formatJSONResponse = (response, statusCode = 200, headers = { 'Content-Type': 'application/json' }) => {
+    return {
+        statusCode,
+        body: JSON.stringify(response),
+        headers,
+    };
+};
+exports.formatJSONResponse = formatJSONResponse;
+
+
+/***/ }),
+
+/***/ "@aws-sdk/client-dynamodb":
+/*!*******************************************!*\
+  !*** external "@aws-sdk/client-dynamodb" ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = require("@aws-sdk/client-dynamodb");
+
+/***/ }),
+
+/***/ "@aws-sdk/lib-dynamodb":
+/*!****************************************!*\
+  !*** external "@aws-sdk/lib-dynamodb" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("@aws-sdk/lib-dynamodb");
+
+/***/ }),
+
+/***/ "uuid":
+/*!***********************!*\
+  !*** external "uuid" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("uuid");
+
+/***/ }),
+
+/***/ "zod":
+/*!**********************!*\
+  !*** external "zod" ***!
+  \**********************/
+/***/ ((module) => {
+
+module.exports = require("zod");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!*****************************!*\
+  !*** ./src/children/put.ts ***!
+  \*****************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.handler = void 0;
+const zod_1 = __webpack_require__(/*! zod */ "zod");
+const util_1 = __webpack_require__(/*! @libs/util */ "./src/libs/util.ts");
+const kid_1 = __webpack_require__(/*! ./kid */ "./src/children/kid.ts");
+const repository_1 = __webpack_require__(/*! @libs/repository */ "./src/libs/repository.ts");
+const put = async (event) => {
+    if (event.body) {
+        try {
+            const item = kid_1.kidSchema.parse(JSON.parse(event.body));
+            await (0, repository_1.saveChildren)(item);
+            console.info('Success - item added or updated');
+            return (0, util_1.formatJSONResponse)({ kid: item }, 201);
+        }
+        catch (err) {
+            if (err instanceof zod_1.ZodError) {
+                return (0, util_1.formatJSONResponse)({ message: 'Error in the request body' }, 400);
+            }
+            return (0, util_1.formatJSONResponse)({ message: err.message }, 500);
+        }
+    }
+    else {
+        return (0, util_1.formatJSONResponse)({ message: 'The body cannot be empty' }, 400);
+    }
+};
+const handler = async (event) => put(event);
+exports.handler = handler;
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
+//# sourceMappingURL=put.js.map
